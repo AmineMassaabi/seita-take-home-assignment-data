feae704596d6a692cdd894a27fe76ee945ac46bf
Amine Massaabi <amine@laevitas.ch>
2024-11-12 01:36:06 +0100

    add WEATHER_FORECAST_MODEL and utils function

---
+++ src/utils/helpers.py
@@ -0,0 +1,85 @@
+import traceback
+from datetime import datetime
+
+import pandas
+import pandas as pd
+from flask import jsonify
+from dateutil.parser import parse
+
+from src.utils.consts import csv_url, THRESHOLDS
+
+
+def handle_request_errors(f):
+    def wrapper(*args, **kwargs):
+        try:
+            print(f'start {f.__name__}')
+            return f(*args, **kwargs)
+        except:
+            error_msg = f'Error on {f.__name__} \n{traceback.format_exc()}'
+            traceback.print_exc()
+            response = {
+                'result': 'failed',
+                'status': 'down',
+                'error': error_msg
+
+            }
+            return jsonify(response), 500
+
+    return wrapper
+
+
+def parse_date(date: str) -> datetime:
+    # return pd.to_datetime(date, yearfirst=True, utc=True).to_pydatetime()
+    return parse(date, yearfirst=True, ignoretz=True)
+
+
+def get_transformed_data_frames(get_online: bool = False) -> [pd.DataFrame]:
+    if get_online:
+        weather_data = pandas.read_csv(csv_url)
+    else:
+        weather_data = pandas.read_csv('weather.csv')
+    transformed_weather_data = data_transform(weather_data)
+    [temperature, irradiance, wind_speed] = data_split(transformed_weather_data)
+    return [temperature, irradiance, wind_speed]
+
+
+def data_transform(df: pd.DataFrame) -> pd.DataFrame:
+    df['event_start'] = pd.to_datetime(df['event_start'])
+    df['exact_time'] = df['event_start'] - pd.to_timedelta(df['belief_horizon_in_sec'], unit='s')
+    df = df.rename({'belief_horizon_in_sec': 'horizon'})
+    return df
+
+
+def data_split(df: pd.DataFrame) -> [pd.DataFrame]:
+    temperature = df[df['sensor'] == 'temperature']
+    irradiance = df[df['sensor'] == 'irradiance']
+    wind_speed = df[df['sensor'] == 'wind speed']
+    return [temperature, irradiance, wind_speed]
+
+
+def extract_nearset_value_to_date(table, given_date):
+    # table = table[table.event_start == given_date]
+    return table.loc[(table['exact_time'] - pd.to_datetime(given_date, utc=True)).abs().idxmin()]['event_value']
+
+
+def get_nearst_values_to_now(temperature, irradiance, wind_speed, now):
+    # looked_date = pd.date_range(start=now, end=now, freq="H")
+    temperature_value = extract_nearset_value_to_date(temperature, now)
+    irradiance_value = extract_nearset_value_to_date(irradiance, now)
+    wind_speed_value = extract_nearset_value_to_date(wind_speed, now)
+
+    return {'temperature': temperature_value, 'irradiance': irradiance_value, 'wind_speed': wind_speed_value}
+
+
+def get_difference_of_hour_between_two_dates(now, then):
+    # looked_date = pd.date_range(start=now, end=now, freq="H")
+    difference = round((then - now).total_seconds() / 3600)
+    return difference
+
+
+def check_threshold(forecasted_results):
+    return {
+        "warm": bool(forecasted_results['temperature'] > THRESHOLDS['warm']),
+        "sunny": bool(forecasted_results['irradiance'] > THRESHOLDS['sunny']),
+        "windy": bool(forecasted_results['wind speed'] > THRESHOLDS['windy'])
+    }
---
+++ src/weather_forecast.py
@@ -0,0 +1,158 @@
+from copy import copy
+from datetime import timedelta
+
+import numpy as np
+import pandas as pd
+from statsmodels.regression.linear_model import OLS
+from timetomodel import speccing, create_fitted_model, ModelState, make_forecast_for
+from timetomodel.featuring import construct_features
+from timetomodel.transforming import Transformation
+import statsmodels.api as sm
+
+from src.utils.consts import target_variables
+
+
+class MyDFPostProcessing(Transformation):
+    def __init__(self, sensor):
+        self.sensor = sensor
+
+    def transform_dataframe(self, df: pd.DataFrame):
+        """Keep the most recent observation, drop duplicates, filter by sensor"""
+
+        df = df[df['sensor'] == self.sensor]
+        return (
+            df.sort_values(by=["belief_horizon_in_sec"], ascending=True)
+            .drop_duplicates(subset=["event_start"], keep="first")
+            .sort_values(by=["event_start"])
+        )
+
+
+def get_speccing_object_series_specs(df, sensor):
+    return speccing.ObjectSeriesSpecs(
+        data=pd.Series(
+            index=df['exact_time'],
+            data=list(df['event_value']),
+        ),
+        name=sensor,
+        resampling_config=dict(
+            downsampling_method="first",
+            event_resolution=timedelta(hours=0),
+        ),
+        interpolation_config=dict(method="pad", limit=3),
+    )
+
+
+def get_speccing_csv_series_specs(sensor):
+    return speccing.CSVFileSeriesSpecs(
+        file_path='weather.csv',
+        time_column='event_start',
+        value_column='event_value',
+        name=sensor,
+        post_load_processing=MyDFPostProcessing(sensor),
+        read_csv_config={'parse_dates': ["event_start"]},
+        # feature_transformation=BoxCoxTransformation(lambda2=0.1)
+    )
+
+
+class WEATHER_FORECAST_MODEL():
+    def __init__(self, temperature_data=None, irradiance_data=None, wind_speed_data=None):
+        # also we can work wih CSVFileSeriesSpecs inplace of ObjectSeriesSpecs
+        # temperature_data_specs = get_speccing_object_series_specs(temperature_data, 'temperature')
+        # irradiance_data_specs = get_speccing_object_series_specs(irradiance_data, 'irradiance')
+        # wind_speed_data_specs = get_speccing_object_series_specs(wind_speed_data, 'wind_speed')
+        # temperature_series = temperature_data_specs.load_series(expected_frequency=timedelta(hours=1))
+        # irradiance_series = irradiance_data_specs.load_series(expected_frequency=timedelta(hours=1))
+        # wind_speed_series = wind_speed_data_specs.load_series(expected_frequency=timedelta(hours=1))
+
+        self.fitted_model = None
+        self.temperature_series = get_speccing_csv_series_specs('temperature').load_series(
+            expected_frequency=timedelta(hours=1))
+        self.temperature_object_series = speccing.ObjectSeriesSpecs(self.temperature_series, name="temperature")
+        self.irradiance_series = get_speccing_csv_series_specs('irradiance').load_series(
+            expected_frequency=timedelta(hours=1))
+        self.irradiance_object_series = speccing.ObjectSeriesSpecs(self.irradiance_series, name="irradiance")
+        self.wind_speed_series = get_speccing_csv_series_specs('wind speed').load_series(
+            expected_frequency=timedelta(hours=1))
+        self.wind_speed_object_series = speccing.ObjectSeriesSpecs(self.wind_speed_series, name="wind speed")
+        self.fitted_models = {}
+
+    def get_model_specs(self, lag=24, target_variable="temperature"):
+        # Define the time range for training and testing data
+        start_of_training = self.temperature_series.index[0]  # First timestamp in the temperature series
+        end_of_testing = self.temperature_series.index[-1]  # Last timestamp in the temperature series
+
+        if target_variable == 'temperature':
+            outcome_var = copy(self.temperature_object_series)
+            regressors = [copy(self.irradiance_object_series), copy(self.wind_speed_object_series)]
+        elif target_variable == 'irradiance':
+            outcome_var = copy(self.irradiance_object_series)
+            regressors = [copy(self.temperature_object_series), copy(self.wind_speed_object_series)]
+        else:
+            outcome_var = copy(self.wind_speed_object_series)
+            regressors = [copy(self.temperature_object_series), copy(self.irradiance_object_series)]
+
+        # todo : test on other regression models ( like VAR for Multivariate Forecasting from statsmodels.tsa.api)
+        model_specs = speccing.ModelSpecs(
+            model=OLS,
+            outcome_var=outcome_var,
+            frequency=timedelta(hours=1),
+            horizon=timedelta(hours=lag),  # Forecast horizon based on calculated lag
+            lags=[lag],  # Use only the lag equal to the forecast horizon
+            regressors=regressors,
+            start_of_training=start_of_training + timedelta(hours=lag),
+            end_of_testing=end_of_testing - timedelta(hours=lag),
+            ratio_training_testing_data=0.90,  # Train-test split ratio
+        )
+        return model_specs
+
+    def train_models(self, lag=24):
+        for target_variable in target_variables:
+            model_specs = self.get_model_specs(lag=lag, target_variable=target_variable)
+            fitted_model = create_fitted_model(model_specs, f"Weather Forecast {target_variable} Model")
+            self.fitted_models[target_variable] = fitted_model
+
+    def evaluate_models(self, lag):
+        """
+        We transformed evaluate_models from timetomodel to get adequate output
+        :return: rmse value
+        """
+        rmse_values = {}
+        for target_variable in target_variables:
+            m1 = ModelState(self.fitted_models[target_variable], self.get_model_specs(lag=lag,
+                                                                                      target_variable=target_variable))
+            fitted_m1, m1_specs = m1.split()
+            regression_frame = construct_features(time_range="test", specs=m1_specs)
+            x_test = regression_frame.iloc[:, 1:]
+            y_test = np.array(regression_frame.iloc[:, 0])
+            try:
+                y_hat_test = fitted_m1.predict(x_test)
+            except TypeError:
+                y_hat_test = fitted_m1.predict(start=x_test.index[0], end=x_test.index[-1], exog=x_test)
+
+            if m1_specs.outcome_var.feature_transformation is not None:
+                y_test = m1_specs.outcome_var.feature_transformation.back_transform_value(y_test)
+                y_hat_test = m1_specs.outcome_var.feature_transformation.back_transform_value(y_hat_test)
+
+            rmse_values[target_variable] = round(sm.tools.eval_measures.rmse(y_test, y_hat_test, axis=0), 4)
+            print(f'rmse value for {target_variable} = {rmse_values[target_variable]}, in percentage ='
+                  f' {round(abs(rmse_values[target_variable] / np.mean(y_test) - 1) * 100)}%')
+        return rmse_values
+
+    def forecast_model(self, now, calculation_mesures, lag=24):
+        self.train_models(lag)
+        features = pd.DataFrame(
+            index=pd.date_range(start=now, end=now, freq="H"),
+            data=calculation_mesures,
+        ).iloc[0]
+
+        forecasted_values = {}
+        for target_variable in target_variables:
+            model_specs = self.get_model_specs(lag, target_variable=target_variable)
+            forecasted_value = make_forecast_for(
+                specs=model_specs,
+                features=features,
+                model=self.fitted_models[target_variable]
+            )
+            forecasted_values[target_variable] = forecasted_value
+        return forecasted_values
+        # self.evaluate_models(lag)
